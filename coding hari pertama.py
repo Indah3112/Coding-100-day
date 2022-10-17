2a4580c64483b3e050be34c2ef458ddc77aa29ae
Indah3112 <indahiw1231@gmail.com>
2022-10-17 16:07:13 +0800

    coding day 1

---
+++ coding hari pertama.py
@@ -0,0 +1,9 @@
+nama = "indah"
+umur = 18
+tinggi = 155.5
+status_mahasiswa =True
+
+print("nama : ",nama)
+print("umur : ",umur)
+print("tinggi : ",tinggi)
+if (status_mahasiswa ==True): print("status mahasiswa : pelajar")
